3d2318dba8d21d148e3bb6a790a35b0e8aef9be9
kristoffer <krissslazarte@gmail.com>
2024-10-31 17:12:29 +0800

    fifth

--- view/dashboard.js
+++ view/dashboard.js
@@ -10,6 +10,7 @@ let cart = []
 document.getElementById('cartContainer').style.visibility = "hidden";
 
 //display all shits
+function display(){
 fetch("/product/products")
   .then((response) => response.json())
   .then((data) =>
@@ -19,38 +20,36 @@ fetch("/product/products")
                         item: item.item,
                         price: item.price,
                         quantity: item.quantity})
-      container.innerHTML += `<div class="productCard" style="border: 2px solid black; width: 150px; height: 220px; ">
-                    <img class="image" src=${item.imageUrl} alt="fruit image" height=100px width="150px"> <br>
-                    <strong> <span class="item"> ${item.item}</span> </strong><br>
-                    Price: <span class="price"> ${item.price}</span> Php <br> 
-                    Quantity: <span class="quantity"> ${item.quantity} </span> Pcs<br>
-                    <br><button class="addCart" >Add to cart</button></div><br>`;
+      container.innerHTML += `<div class="productCard"  ">
+                    <img class="image" src=${item.imageUrl} alt="product image"> 
+                    <span class="item"> ${item.item}</span> 
+                    Price: <span class="price">${item.price}</span> Php 
+                    Quantity: <span class="quantity">${item.quantity}</span>Pcs
+                    <button class="addCart" >Add to cart</button></div>`;
     })
   )
   .catch((error) => console.error("Error:", error));
-
+}
+  display();
   //search shits
 searchBtn.onclick =() => {
     let itemSearch = document.getElementById('itemSearch').value
     let lowerCased = itemSearch.toLowerCase().trim()
-    
 
     if(lowerCased ==''){
       alert('field is empty')
-    
     }else{
-      
       const find = prodHolder.find(prodHolder => prodHolder.item === lowerCased);
       if(find ===undefined){
         alert('Can\'t find item')
       }else{
       container.innerHTML=``;
-      container.innerHTML = `<div class="productCard" style="border: 2px solid black; width: 150px; height: 220px;  ">
-                    <img class="image" src=${find.imageUrl} alt="fruit image" height=100px width="150px"> <br>
-                    <strong><span class="item"> ${find.item}</span></strong><br>
-                    Price:<span class="price"> ${find.price}</span> Php <br> 
-                    Quantity:<span class="quantity"> ${find.quantity}</span> Pcs<br>
-                    <br><button class="addCart">Add to cart</button></div><br>`;
+      container.innerHTML = `<div class="productCard" ">
+                    <img class="image" src=${find.imageUrl} alt="product image" > 
+                    <span class="item"> ${find.item}</span>
+                    Price:<span class="price"> ${find.price}</span> Php  
+                    Quantity:<span class="quantity"> ${find.quantity}</span> Pcs
+                    <button class="addCart">Add to cart</button></div><br>`;
       }
   }
   
@@ -58,14 +57,7 @@ searchBtn.onclick =() => {
 //display shits again
 back.onclick =() =>{
   container.innerHTML=``
-  prodHolder.forEach(one => {
-    container.innerHTML += `<div class="productCard" style="border: 2px solid black; width: 150px; height: 220px;  ">
-                    <img class="image" src=${one.imageUrl} alt="fruit image" height=100px width="150px"> <br>
-                    <strong><span class="item"> ${one.item}</span></strong><br>
-                    Price:<span class="price"> ${one.price}</span> Php <br> 
-                    Quantity:<span class="quantity"> ${one.quantity}</span> Pcs<br>
-                    <br><button  class="addCart" >Add to cart</button></div><br>`;
-  })
+  display();
 }
 
 //add to cart
@@ -78,20 +70,17 @@ container.addEventListener('click', function (e) {
     const item = productCard.querySelector('.item').textContent;
     const price = productCard.querySelector('.price').textContent;
 
-  
-    let quantity = 0;
-
     if (!cart.some(cartItem => cartItem.item === item)) {
       
       const cartItem = { imageUrl: img, item: item, price: price, quantity: 1 };
       cart.push(cartItem);
       
-      cartContainer.innerHTML += `<div class="productCart" style="border: 2px solid black; width: 150px; height: 220px;">
-          <img class="image" src="${cartItem.imageUrl}" alt="product image" height="100px" width="150px"> <br>
-          <strong><span class="item">${cartItem.item}</span></strong><br>
-          Quantity: <span class="quantity">${cartItem.quantity}</span><br><br>
-          Total Amount: <strong><span class="price">${cartItem.price}</span></strong> Php <br> 
-          <br><button class="remove">Remove</button> <button class="order">Order</button></div><br>`;
+      cartContainer.innerHTML += `<div class="productCart" >
+          <img class="image" src="${cartItem.imageUrl}" alt="product image" >
+          <span class="item">${cartItem.item}</span>
+          Quantity: <span class="quantity">${cartItem.quantity}</span>
+          Total Amount: <span class="price">${cartItem.price}</span> Php
+          <button class="remove">Remove</button> <button class="order">Order</button></div>`;
     } else {
       
       const existingCartItem = cart.find(cartItem => cartItem.item === item);
@@ -122,7 +111,6 @@ cartContainer.addEventListener('click', function (e) {
     cart.pop({item:itemSelected})
     removeCard.remove()
     console.log(cart)
-    
       }
     })
 
@@ -132,23 +120,25 @@ cartButton.onclick = ()=> {
   document.getElementById('cartContainer').style.visibility = "hidden";
   click += 1
   }else{
-    
   document.getElementById('cartContainer').style.visibility = "visible";
   click += 1
   }
 }
+
+
 //order route
 cartContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('order')) {
     let order = e.target.closest('.productCart'); 
-  
+    
     const itemSelected = order.querySelector('.item').textContent;
-
     console.log(`${itemSelected} is ordered biatch`)
 
-    
     cart.pop({item:itemSelected})
     console.log(`remaining items in the cart`, cart)
     order.remove()
       }
     })
+
+   
+  
