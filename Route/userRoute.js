e387dafa0447d7c112735cc943a1bc5e24905439
kristoffer <krissslazarte@gmail.com>
2025-01-20 07:52:33 +0800

    one last

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -6,23 +6,23 @@ const {getUser ,getOneUser, createUser, deleteUser, update ,addCart, updateCart,
 const router = express();
 
 
-router.post('node-app-y9kl.onrender.com/register', createUser);
+router.post('/node-app-y9kl.onrender.com/register', createUser);
 
-router.get('node-app-y9kl.onrender.com/', getUser);
+router.get('/node-app-y9kl.onrender.com/', getUser);
 
-router.post('node-app-y9kl.onrender.com/login', getOneUser);
+router.post('/node-app-y9kl.onrender.com/login', getOneUser);
 
-router.post('node-app-y9kl.onrender.com/logged',userLogged);
+router.post('/node-app-y9kl.onrender.com/logged',userLogged);
 
-router.put('node-app-y9kl.onrender.com/update', update);
+router.put('/node-app-y9kl.onrender.com/update', update);
 
-router.post('node-app-y9kl.onrender.com/cart',addCart);
+router.post('/node-app-y9kl.onrender.com/cart',addCart);
 
-router.put('node-app-y9kl.onrender.com/updateCart',updateCart);
+router.put('/node-app-y9kl.onrender.com/updateCart',updateCart);
 
-router.delete('node-app-y9kl.onrender.com/deleteItem', deleteItem)
+router.delete('/node-app-y9kl.onrender.com/deleteItem', deleteItem)
 
-router.delete('node-app-y9kl.onrender.com/orderItem', orderItem)
+router.delete('/node-app-y9kl.onrender.com/orderItem', orderItem)
 
 
 // unused
