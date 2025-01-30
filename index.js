42e523b1e9d62306c67bc7ddbfb9546958aea52d
kristoffer <krissslazarte@gmail.com>
2025-01-30 17:28:18 +0800

    last

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const mongoose = require('mongoose')
 const UserRoute = require('./Route/userRoute');
 const ProductRoute = require('./Route/productRoute');
 require ('dotenv').config()
-const cors = require('cors')
-const path = require('path');
+
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('view'))
-
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'index.html'));
-});
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3001;
 
-
 mongoose.connect(MONGO_URL)
-    .then(() => {
-        console.log('Connected to MongoDB!');
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Database connection error:', error);
-    });
+.then(() =>{app.listen(PORT,() =>{
+    console.log(`to port ${PORT}`);
+}),
+console.log('Connected!')},
 
+).catch((error)=>{
+    console.log(error)}
+)
+
+app.use(express.json())
+
+app.use(express.static('view'))
+
+// app.post('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'view', 'index.html'));
+// });
+
+// app.post('/dashboard.html', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'view', 'dashboard.html'));
+// });
 
 app.use('/user',UserRoute)
+
 app.use('/product', ProductRoute)
 
 module.exports = app;
